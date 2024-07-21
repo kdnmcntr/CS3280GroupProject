c1fb595a4d4b3b00534ed04fe69c8bf54d9c6c6b
kdnmcntr <kaden.mcentire@gmail.com>
2024-07-20 21:24:03 -0600

    clsMainSQL
    added prototype SQL statements into clsMainSQL

--- Main/clsMainSQL.cs
+++ Main/clsMainSQL.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using GroupProject_Car_Dealership_Prototype.Common;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,38 @@ namespace GroupProject_Car_Dealership_Prototype.Main
 {
     internal class clsMainSQL
     {
+        clsDataAccess db;
+        DataSet ds;
+
+        public clsMainSQL()
+        {
+            db = new clsDataAccess();
+            ds = new DataSet();
+        }
+
+        public DataSet getItems()
+        {
+
+            int retVal = 0;
+
+            ds = db.ExecuteSQLStatement("SELECT * FROM Items", ref retVal);
+            return ds;
+        }
+
+        public void addItem(DataSet itm)
+        {
+            int retVal = 0;
+
+            db.ExecuteSQLStatement("INSERT INTO Items (CarModel, CarCode, Price) VALUES ({model}, {code}, {price})", ref retVal);
+        }
+
+        public void deleteItem(DataSet itm)
+        {
+            int retVal = 0;
+
+            db.ExecuteSQLStatement("DELETE FROM Items WHERE {code} = CarCode", ref retVal);
+        }
+
+
     }
 }
